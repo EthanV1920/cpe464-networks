310fa3c56d58f0c87ef69b431642b887e18ce167
Ethan Vosburg <63841608+EthanV1920@users.noreply.github.com>
2025-03-02 13:07:55 -0800

    Program 3: variables are not being passed correctly working on fixing that

--- program3/rcopy.c
+++ program3/rcopy.c
@@ -40,7 +40,7 @@
 typedef struct {
     char fromFileName[100];
     char toFileName[100];
-    uint16_t windowSize;
+    uint32_t windowSize;
     uint16_t bufferSize;
     float errorRate;
     char remoteMachine[64];
@@ -59,27 +59,29 @@ typedef struct {
 
 void talkToServer(int socketNum, struct sockaddr_in6 *server);
 int readFromStdin(char *buffer);
-setupInfo_t processArgs(int argc, char *argv[]);
-int connectString(setupInfo_t setupInfo);
+void processArgs(int argc, char *argv[], setupInfo_t *setupInfo);
+int connectString(setupInfo_t *setupInfo);
 void printBuf(uint8_t *buf, uint16_t len);
-
 
 int main(int argc, char *argv[]) {
     int socketNum = 0;
     struct sockaddr_in6 server; // Supports 4 and 6 but requires IPv6 struct
     setupInfo_t setupInfo;
 
-    setupInfo = processArgs(argc, argv);
+    processArgs(argc, argv, &setupInfo);
 
-    socketNum = setupUdpClientToServer(&server, argv[REMOTE_MACHINE],
-                                       setupInfo.remotePort);
-
-    sendErr_init(setupInfo.errorRate, DROP_ON, FLIP_OFF, DEBUG_ON, RSEED_ON);
-
-    connectString(setupInfo);
-    talkToServer(socketNum, &server);
-
-    close(socketNum);
+    printf("DEBUG: SetupInfo %d %d\n", setupInfo.bufferSize,
+           setupInfo.windowSize);
+    printBuf((uint8_t *)&setupInfo, sizeof(setupInfo_t));
+    connectString(&setupInfo);
+    // socketNum = setupUdpClientToServer(&server, argv[REMOTE_MACHINE],
+    //                                    setupInfo.remotePort);
+    //
+    // sendErr_init(setupInfo.errorRate, DROP_ON, FLIP_OFF, DEBUG_ON, RSEED_ON);
+    //
+    // talkToServer(socketNum, &server);
+    //
+    // close(socketNum);
 
     return 0;
 }
@@ -131,10 +133,7 @@ int readFromStdin(char *buffer) {
     return inputLen;
 }
 
-setupInfo_t processArgs(int argc, char *argv[]) {
-
-    setupInfo_t args;
-
+void processArgs(int argc, char *argv[], setupInfo_t *setupInfo) {
     /* check command line arguments  */
     if (argc != 8) {
         printf("usage: %s from-filename to-filename window-size buffer-size"
@@ -151,48 +150,52 @@ setupInfo_t processArgs(int argc, char *argv[]) {
         exit(1);
     }
 
-    printf("Hello \n");
-    strncpy(args.fromFileName, argv[FROM_FILENAME], 100);
-    strncpy(args.toFileName, argv[TO_FILENAME], 100);
-    args.windowSize = atoi(argv[WINDOW_SIZE]);
-    args.bufferSize = atoi(argv[BUFFER_SIZE]);
-    args.errorRate = atof(argv[ERROR_RATE]);
-    strncpy(args.remoteMachine, argv[REMOTE_MACHINE], 64);
-    args.remotePort = atoi(argv[REMOTE_PORT]);
+    strncpy(setupInfo->fromFileName, argv[FROM_FILENAME], 100);
+    strncpy(setupInfo->toFileName, argv[TO_FILENAME], 100);
+    setupInfo->windowSize = atoi(argv[WINDOW_SIZE]);
+    setupInfo->bufferSize = atoi(argv[BUFFER_SIZE]);
+    setupInfo->errorRate = atof(argv[ERROR_RATE]);
+    strncpy(setupInfo->remoteMachine, argv[REMOTE_MACHINE], 64);
+    setupInfo->remotePort = atoi(argv[REMOTE_PORT]);
 
-    printf("DEBUG: %s %s %d %d %f %s %d\n", args.fromFileName, args.toFileName,
-           args.windowSize, args.bufferSize, args.errorRate, args.remoteMachine,
-           args.remotePort);
+    // int setupInfoSz = 100 + 100 + 4 + 2 + 4 + 64 + 2;
+    // printBuf((uint8_t *)&setupInfo, setupInfoSz);
 
-    return args;
+    printf("DEBUG: %s %s %d %d %f %s %d\n", setupInfo->fromFileName,
+           setupInfo->toFileName, setupInfo->windowSize, setupInfo->bufferSize,
+           setupInfo->errorRate, setupInfo->remoteMachine,
+           setupInfo->remotePort);
 }
 
-int connectString(setupInfo_t setupInfo) {
-    printf("DEBUG: Setting up connection to server");
-    uint16_t bufferSize = 4+2+1+100+2+4;
+int connectString(setupInfo_t *setupInfo) {
+    printf("DEBUG: Setting up connection to server %d %d \n",
+           setupInfo->bufferSize, setupInfo->windowSize);
+    uint16_t bufferSize = 4 + 2 + 1 + 100 + 2 + 4;
     uint8_t buf[bufferSize];
-    memset(buf, 0x0000, 4);
-    memset(buf + 4, 0xFF, 2);
-    memset(buf + 6, 0x8, 1);
-    memcpy(buf + 7, setupInfo.fromFileName, 100);
-    memset(buf + 107, setupInfo.bufferSize, 2);
-    memset(buf + 109, setupInfo.windowSize, 4);
-    
+    memset(buf, 0x0, 4);
+    memset(buf + 4, 0xF, 2);
+    buf[6] = 0x8;
+    memcpy(buf + 7, setupInfo->fromFileName, 100);
+    uint32_t translatedWinSz = htonl(setupInfo->windowSize);
+    uint32_t translatedBufSz = htonl(setupInfo->bufferSize);
+    memcpy(buf + 107, (char *)&translatedWinSz, sizeof(uint32_t));
+    memcpy(buf + 111, (char *)&translatedBufSz, sizeof(uint16_t));
+
     printBuf(buf, bufferSize);
 
     return 1;
 }
 
 void printBuf(uint8_t *buf, uint16_t len) {
-  printf("%d: 00 00", len);
-  for (int i = 0; i < len; i++) {
-    if (buf[i] >= 32 && buf[i] <= 127) {
-      printf("%c", (char)buf[i]);
-    } else {
-      printf(" %hhu ", buf[i]);
+    printf("Length: %d\n", len);
+    int r = 0;
+    for (int i = 0; i < len; i++) {
+        i % 8 == 0 ? printf("\n%d\t| ", r++) : printf("");
+        if (buf[i] >= 32 && buf[i] <= 127) {
+            printf("%c\t", (char)buf[i]);
+        } else {
+            printf("%hhu\t", buf[i]);
+        }
     }
-  }
-  printf("\n");
+    printf("\n");
 }
-
-
